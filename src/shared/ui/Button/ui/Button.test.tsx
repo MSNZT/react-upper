57bd08bc22d1208689ed938f0c5c62e9dc57c0a4
MSNZT <shamyphy@gmail.com>
2023-12-19 23:12:22 +0300

    update stylelint, corrected unit tests

--- src/shared/ui/Button/ui/Button.test.tsx
+++ src/shared/ui/Button/ui/Button.test.tsx
@@ -3,14 +3,14 @@ import {Button, ButtonTheme} from "@/shared/ui/Button/ui/Button";
 
 describe('Button', () => {
   test('test', () => {
-    render(<Button>Check</Button>);
+    render(<Button data-testid='btn-test'>Check</Button>);
     const btn = screen.getByTestId('btn-test');
     expect(btn).toBeInTheDocument()
     screen.debug()
   })
   
   test('check with add theme', () => {
-    render(<Button theme={ButtonTheme.CLEAR}>Check</Button>);
+    render(<Button data-testid='btn-test' theme={ButtonTheme.CLEAR}>Check</Button>);
     const btn = screen.getByTestId('btn-test');
     expect(btn).toHaveClass('clear')
     screen.debug()
